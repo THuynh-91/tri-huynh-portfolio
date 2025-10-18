ccbe6351891e1023474ae40b450a6aa517797290
THuynh-91 <triqhuynh91@gmail.com>
2025-10-18 01:18:52 -0400

    Add assetPrefix for GitHub Pages

--- next.config.js
+++ next.config.js
@@ -1,11 +1,15 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production'
+const repoName = 'tri-huynh-portfolio'
+
 const nextConfig = {
   reactStrictMode: true,
   output: 'export',
   images: {
     unoptimized: true,
   },
-  basePath: process.env.NODE_ENV === 'production' ? '/tri-huynh-portfolio' : '',
+  basePath: isProd ? `/${repoName}` : '',
+  assetPrefix: isProd ? `/${repoName}/` : '',
 }
 
 module.exports = nextConfig
