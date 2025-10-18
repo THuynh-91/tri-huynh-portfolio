1a3fe865ad3e69380aee2680893c3a7070501dbf
THuynh-91 <triqhuynh91@gmail.com>
2025-10-18 01:10:15 -0400

    Fix basePath to match repository name

--- next.config.js
+++ next.config.js
@@ -5,7 +5,7 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-  basePath: process.env.NODE_ENV === 'production' ? '/Portfolio_Website' : '',
+  basePath: process.env.NODE_ENV === 'production' ? '/tri-huynh-portfolio' : '',
 }
 
 module.exports = nextConfig
